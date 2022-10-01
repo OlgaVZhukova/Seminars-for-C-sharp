8801ed47578c07746b06cc6209f7cedd4fe73b42
Olga Zhukova <olga.v.zhukoff@gmail.com>
2022-10-01 23:54:58 +0300

    Task0 added

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая на вход 
+// принимает число и выдаёт его квадрат (число 
+// умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int value = Prompt("Введите число >");
+int square = value*value;
+System.Console.WriteLine($"Квадрат числа {value} равен {square}");
+
